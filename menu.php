f67c007fcd2ea19cb5224ba8627b0635c2eb460b
Adan Mojo <adanmojo@gmail.com>
2023-11-15 15:35:24 -0300

    Servicios Profesionales

--- menu.php
+++ menu.php
@@ -5,8 +5,8 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Inicio</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Servicios Profesionales</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="servicios.php">Servicios Profesionales</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Pizzerias del Mundo</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Quienes Somos</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Contacto</a></li>
---
+++ servicios.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="Asesoria, Consultoria, Desarrollo de Productos, Estandarizacion de Recetas, Produccion de Empanadas, Produccion de Pizzas, Analisis de Costos, Analisis de procesos..." />
+        <meta name="author" content="Miguel Mojo" />
+        <title>TODOPIZZA - Asesoria y Consultoria</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Menu -->
+        <?php include('menu.php');?>
+
+        <!-- Page Content-->
+        <div class="container px-4 px-lg-5">
+            <!-- Introduccion -->
+            <div class="card text-white bg-secondary my-5 py-4">
+                <div class="row">
+                <div class="col-12 m-auto"><p class="p-3">
+                Brindamos asesoria y consultoria a las empresas dedicadas a la elaboración de empanadas y pizzas. Dentro de nuestros servicios se encuentran todos los aspectos a tener en cuenta al momento de comenzar o seguir un emprendimiento de este rubro. 
+                Algunas de las preguntas que podemos responder son: ¿Necesito mejorar mi negocio?, ¿Como optimizo mi empresa y mejoro la renta?, ¿Deberia innovar procesos o renovar productos?, ¿Me conviene convertirme en franquicia? entre otras.
+                </p></div>
+                </div>
+            </div>
+
+            <!-- Content Row-->
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <img src="assets/maquinaria-inicio.png" class="card-img-top" alt="...">
+                        <div class="card-body bg-light">
+                            <h4 class="card-title">Asesoria</h4>
+                            <p class="card-text">Contamos con una basta experiencia con el uso de diferentes maquinarias dependiendo de la produccion deseada en el que podemos asesorar.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <img src="assets/maquinaria-inicio.png" class="card-img-top" alt="...">    
+                        <div class="card-body bg-light">
+                            <h4 class="card-title">Consultoria</h4>
+                            <p class="card-text">Confeccionamos recetas segun las pretenciones adaptando al proceso productivo y maquinaria</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <img src="assets/maquinaria-inicio.png" class="card-img-top" alt="...">
+                        <div class="card-body bg-light">
+                            <h4 class="card-title">Nuevos desarrollos</h4>
+                            <p class="card-text">Realizamos el diseño de los procesos de elaboración para poder optimizar y estandarizar la producción.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Content Row-->
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <img src="assets/proceso-inicio.png" class="card-img-top" alt="...">
+                        <div class="card-body bg-light">
+                            <h4 class="card-title">Estanndarización</h4>
+                            <p class="card-text">Contamos con una basta experiencia con el uso de diferentes maquinarias dependiendo de la produccion deseada en el que podemos asesorar.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <img src="assets/proceso-inicio.png" class="card-img-top" alt="...">
+                        <div class="card-body bg-light">
+                            <h4 class="card-title">Innovación</h4>
+                            <p class="card-text">Confeccionamos recetas segun las pretenciones adaptando al proceso productivo y maquinaria</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <img src="assets/proceso-inicio.png" class="card-img-top" alt="...">
+                        <div class="card-body bg-light">
+                            <h4 class="card-title">Profesionalismo</h4>
+                            <p class="card-text">Realizamos el diseño de los procesos de elaboración para poder optimizar y estandarizar la producción.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
