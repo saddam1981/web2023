2cde14e9324db48b9063456aa9a3d2d41b384e30
Adan Mojo <adanmojo@gmail.com>
2023-11-15 14:57:36 -0300

    Inicial

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="Una empresa dedicada a la Pizza y la Empanada - Asesoramiento integral en pizzas y empanadas - Consultora - Desarrollo integral de Pizzerías y Casa de empanadas - Cadenas - Franquicias - Manuales operativos- Auditoria de Productos - Formación - Capacitación laboral - Desarrollo de productos y conceptos - Estandarización de recetas y procesos - Recetas y Costos - Pizza al molde, Pizza a la piedra, Piadinas, Focaccias, Panini, Cursos de elaboración de Pizzas, elaboración de Empanadas." />
+        <meta name="author" content="Miguel Mojo" />
+        <title>TODOPIZZA - Asesoria y Consultoria</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Menu -->
+        <?php include('menu.php');?>
+
+        <!-- Page Content-->
+        <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="assets/empanadas-inicio.png" alt="Elaboracion Empanadas" /></div>
+                <div class="col-lg-5">
+                    <h1 class="font-weight-light">Asesoria y Consultoria</h1>
+                    <p>Nos dedicamos a asesorar empresas dedicadas a la empanada y la pizza que tengan el deseo de estandarizar, automatizar y mejorar su producción.</p>
+                    <a class="btn btn-danger" href="#!">Conoce nuestros servicios</a>
+                </div>
+            </div>
+            <!-- Call to Action-->
+            <div class="card text-white bg-secondary my-5 py-4">
+                <div class="row">
+                <div class="col-2 m-auto" style="vertical-align:center;"><img src="assets/Libro003.png" height="180" alt=""></div>
+                <div class="col-10"><p class="p-3">
+                La idea de hacer este libro es poder compartir con ustedes mi experiencia personal en el negocio de las pizzas y empanadas, así como también mi experiencia profesional en el asesoramiento a emprendedores para llevar adelante negocios del rubro. Esta tarea profesional ha sido síntesis de mi historia pizzera y mi eterna curiosidad por profundizar en las entrañas de las cosas para poder ver como se vinculan con los negocios y así facilitar el éxito de la empresa emprendedora. 
+                </br ></br ><a class="btn btn-danger" href="http://www.mercadolibre.com.ar">Comprar por Mercadolibre</a>
+                </p></div>
+                </div>
+        </div>
+            <!-- Content Row-->
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Maquinaria</h2>
+                            <img src="assets/maquinaria-inicio.png" width="100%" alt="">
+                            <p class="card-text">Contamos con una basta experiencia con el uso de diferentes maquinarias dependiendo de la produccion deseada en el que podemos asesorar.</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-danger btn-sm" href="#!">Mas informarción</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Recetas</h2>
+                            <img src="assets/home-recetas.png" width="100%" alt="">
+                            <p class="card-text">Confeccionamos recetas segun las pretenciones adaptando al proceso productivo y maquinaria</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-danger btn-sm" href="#!">Mas informarción</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Procesos</h2>
+                            <img src="assets/proceso-inicio.png" width="100%" alt="">
+                            
+                            <p class="card-text">Realizamos el diseño de los procesos de elaboración para poder optimizar y estandarizar la producción.</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-danger btn-sm" href="#!">More Info</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
